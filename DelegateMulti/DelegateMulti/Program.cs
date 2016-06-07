a002b5712cf6dd580a43a68684ae70213d16e80b
Hussen Fakhrulddin <hf556482@gmail.com>
2016-06-06 22:31:08 -0700

    init

---
+++ DelegateMulti/DelegateMulti/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DelegateMulti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A.Run();
+            Console.ReadLine();
+        }
+
+        delegate int Calculator(int n, int m);
+
+        static class A
+        {
+            public static void Run()
+            {
+                Calculator c = new Calculator((n, m) => n + m);
+                c += new Calculator((n, m) => n * m);
+                Console.WriteLine(c(c(2, 5), 4));
+            }
+        }
+    }
+}
